4ab27c31137d53272ab9d66eb7d89f7f31d9d3ae
Pedro Consuegra <pedroconsuegrat@gmail.com>
2023-01-19 06:19:25 -0500

    new help

--- src/views/create.blade.php
+++ src/views/create.blade.php
@@ -21,8 +21,9 @@
 				<ul>
 
 					<li>Each Laravel integration is created in a separate folder</li>
-					<li>You can have more than one Laravel application integrated with your WordPress site. >WordPress Pete makes it possible for you to access all WordPress data and logic from your each Laravel application</li>
-					<li>To see more tutorials about how WordPress Pete works go to the <a href="https://github.com/peterconsuegra/wordpress-pete-docker/wiki">WIKI</a></li>
+					<li>You can have more than one Laravel application integrated with your WordPress site. WordPress Pete makes it possible for you to access all WordPress data and logic from your each Laravel application</li>
+					<li>The "Laravel app name" is used to create the URL of the Laravel application and depends on the "Laravel Integration Type", for "Same domain" it would be <i>mywordpressite.com/myapp</i> and for "Separate Subdomain" it would be: <i>myapp.mywordpressite.com</i></li>
+					<li>To see WordPress + Laravel tutorials <a href="https://wordpresspete.com/tutorials/">click here.</a></li>
 				</ul>
 				
 				
@@ -77,9 +78,7 @@
 				<p>Import a Laravel instance from a Git repository</p>
 				<ul>
 					<li>WordPress+Laravel is only available for Laravel 5.5+</li>
-					<li>To import a repository that starts with https:// please make sure it's public</li>
-					<li>To import a repository that starts with @git make sure you have added the ssh keys of your machine to the repository account. Go to /ssh/id_rsa.pub and copy and paste to your repository access keys</li>
-						
+					<li>To import a Laravel project, ensure it has public read permissions and use the URL that starts with HTTPS://. You can then put the project back in private if necessary.</li>
 				</ul>
 			</div>
 		</div>
@@ -91,7 +90,7 @@
 					
 			<div class="form-group import_git_fields" id="wordpress_laravel_git" style="display: none;">
 			
-				<input type="text" id="wordpress_laravel_git-field" name="wordpress_laravel_git" class="form-control" placeholder="git@github.com:peterconsuegra/example.git" />
+				<input type="text" id="wordpress_laravel_git-field" name="wordpress_laravel_git" class="form-control" placeholder="https://github.com/peterconsuegra/example.git" />
                    
 				<div id="wordpress_laravel_git_error_area"> 
 				</div>
@@ -120,7 +119,7 @@
 			
 			<select class="form-control" id="integration_type-field" name="integration_type">';
 			 <option value="">Laravel Integration Type</option>
-			 <option value="inside_wordpress">Inside WordPress</option>
+			 <option value="inside_wordpress">Same domain</option>
 			 <option value="separate_subdomain">Separate subdomain</option>
 		 	</select>
 			
@@ -171,9 +170,9 @@
 	
 	$("#integration_type-field").change(function() {
 		if($(this).val()=="inside_wordpress"){
-			input = '<input type="text" id="wordpress_laravel_name-field" placeholder="WordPress route" name="wordpress_laravel_name" class="form-control inline_class url_wordpress_laravel" />';
+			input = '<input type="text" id="wordpress_laravel_name-field" placeholder="Laravel app name" name="wordpress_laravel_name" class="form-control inline_class url_wordpress_laravel" />';
 		}else{
-			input = '<input type="text" id="wordpress_laravel_name-field" placeholder="Subdomain" name="wordpress_laravel_name" class="form-control inline_class url_wordpress_laravel" />';
+			input = '<input type="text" id="wordpress_laravel_name-field" placeholder="Laravel app name" name="wordpress_laravel_name" class="form-control inline_class url_wordpress_laravel" />';
 		}
 		$("#integration_param").html(input);
 	});
