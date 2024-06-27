69d072b07da426a74bcaefbfd394200e63062f81
Pedro Consuegra <pedroconsuegrat@gmail.com>
2024-06-27 14:11:44 +0000

    some changes

--- src/views/create.blade.php
+++ src/views/create.blade.php
@@ -59,11 +59,11 @@
 				
 				<select class="form-control" id="selected_version" name="selected_version">
 					<option value="">Select Laravel version</option>
-					<option value="5.8">5.8</option>
 					<option value="6.*">6.*</option>
 					<option value="7.*">7.*</option>
 					<option value="8.*">8.*</option>
-					
+					<option value="9.*">9.*</option>
+					<option value="10.*">10.*</option>
 				</select>	
 				
 			</div>
