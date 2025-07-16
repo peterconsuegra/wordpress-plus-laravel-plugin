01a4fae6f7d0098cc1c28661243fbb51d1841a14
Pedro Consuegra <pedroconsuegrat@gmail.com>
2025-07-15 20:08:22 -0600

    refine error

--- wordpress-importer-plugin/src/routes/web.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-//php artisan route:list
-
-Route::get('import_wordpress', 'Pete\WordPressImporter\Http\WordPressImporterController@create')->middleware(['web']);
-
-Route::post('import_wordpress/store', 'Pete\WordPressImporter\Http\WordPressImporterController@store')->middleware(['web']);
-
